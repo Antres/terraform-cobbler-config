d861b5cbaf44b94adb0cc43b33b4541b169f5d23
Alberto Clemente <albertoclemente1983@gmail.com>
2020-05-25 12:53:09 +0200

    Update repos.tf

--- repos.tf
+++ repos.tf
@@ -1,8 +1,9 @@
 resource "cobbler_repo" "my_repo" {
   name           = "test"
+  
   breed          = "yum"
   arch           = "x86_64"
-  mirror         = "http://10.0.1.1/centos/"
+  mirror         = "http://@@http_server@@/centos"
   
   mirror_locally = false
   keep_updated = true
