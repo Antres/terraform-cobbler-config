e63564551998ba4a4cdf19e1e572c0903d51d716
Alberto Clemente <albertoclemente1983@gmail.com>
2020-05-25 18:04:51 +0200

    Update repos.tf

--- repos.tf
+++ repos.tf
@@ -1,45 +1,31 @@
-locals {
-  defaults_repo = {
-    arch = "x86_64",
-    breed = "yum",
-    mirrored = false,
-    updated = true,
-    comment = ""
-  }
-  
-  __repos_default_values = {
-    link                          = null
+locals {  
+  __repos_default_values = {                          # https://cobbler.readthedocs.io/en/latest/cobbler.html#cobbler-repo
+    link                          = null              # mirror
     
-    comment                       = "",
-    arch                          = "x86_64",
-    breed                         = "yum",
-    local                         = false,
-    update                        = true
+    description                   = "",               # comment
+    arch                          = "x86_64",         # arch
+    breed                         = "yum",            # breed
+    
+    local                         = false,            # mirror-locally
+    update                        = true              # keep-updated
   }
   
   repos = {
-    pippo = {
-      link = "http://127.0.0.1/"
-    }
   }
+  
 }
 
 resource "cobbler_repo" "repos" {
+                                                            # Override defaults with input values
   for_each                = { for k, v in local.repos: k => merge(local.__repos_default_values, v) }
   
     name                  = format("%s", each.key)
-#   mirror                = local.repos[each.value].mirror
     mirror                = each.value.link
   
-#   comment               = try(local.repos[each.value].comment, local.defaults_repo.comment)
     comment               = each.value.comment
-#   arch                  = try(local.repos[each.value].arch,local.defaults_repo.arch)
     arch                  = each.value.arch
-#   breed                 = try(local.repos[each.value].breed,local.defaults_repo.breed)
     breed                 = each.value.breed
   
-#   mirror_locally        = try(local.repos[each.value].mirrored, local.defaults_repo.mirrored)
     mirror_locally        = each.value.local
-#   keep_updated          = try(local.repos[each.value].updated,local.defaults_repo.updated)
     keep_updated          = each.value.update
 }
