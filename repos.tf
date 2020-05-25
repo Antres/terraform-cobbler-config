0ee3b670ac9bc63719362309c9613ca8644d9d9f
Alberto Clemente <albertoclemente1983@gmail.com>
2020-05-25 17:37:30 +0200

    Update repos.tf

--- repos.tf
+++ repos.tf
@@ -7,20 +7,44 @@ locals {
     comment = ""
   }
   
+  __repos_default_item = {
+    comment             = "",
+    arch                = "x86_64",
+    breed               = "yum",
+    local               = false,
+    updated             = true
+  }
+  
   repos = {}
 }
 
-resource "cobbler_repo" "my_repo" {
+resource "cobbler_repo" "repos" {
   for_each = toset(keys(local.repos))
   
-  name           = each.value
+    name                  = format("%s",
+                                          each.value
+                            )
+    comment               = try(
+                                  local.repos[each.value].comment,
+                                  local.defaults_repo.comment
+                            )
+    arch                  = try(
+                                  local.repos[each.value].arch,
+                                  local.defaults_repo.arch
+                            )
+    
+    breed                 = try(
+                                  local.repos[each.value].breed,
+                                  local.defaults_repo.breed
+                                )
+    mirror                = local.repos[each.value].mirror
   
-  breed          = try(local.repos[each.value].breed, local.defaults_repo.breed)
-  arch           = try(local.repos[each.value].arch, local.defaults_repo.arch)
-  mirror         = local.repos[each.value].mirror
-  
-  mirror_locally = try(local.repos[each.value].mirrored, local.defaults_repo.mirrored)
-  keep_updated = try(local.repos[each.value].updated, local.defaults_repo.updated)
-  
-  comment = try(local.repos[each.value].comment, local.defaults_repo.comment)
+    mirror_locally        = try(
+                                  local.repos[each.value].mirrored,
+                                  local.defaults_repo.mirrored
+                                )
+    keep_updated          = try(
+                                  local.repos[each.value].updated,
+                                  local.defaults_repo.updated
+                            )
 }
