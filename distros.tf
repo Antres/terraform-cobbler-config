d2b3ec6ed99210ef603de94474a440a04e1d0d2f
Alberto Clemente <albertoclemente1983@gmail.com>
2020-05-25 20:25:05 +0200

    Update distros.tf

--- distros.tf
+++ distros.tf
@@ -1,12 +1,52 @@
 locals {
-  __repos_default_values = {}
+  __repos_default_values = {
+    boot = {
+      kernel                  = null,
+      options                 = {},
+      initrd                  = null,
+      others                  = [],
+    },
+    
+    description               = "",
+    
+    arch                      = "x86_64",
+    breed                     = "redhat",
+    os_version                = "other",
+    
+    owners                    = ["admin"],
+    
+    cms = {
+      roles                   = [],
+      templates               = {},
+    },
+    
+    redhat_management = {
+      key                     = "",
+      server                  = "",
+    },
+  }
+  
+  distros = {}
 }
 
 resource "cobbler_distro" "distros" {
-  name       = "foo"
-  breed      = "ubuntu"
-  os_version = "trusty"
-  arch       = "x86_64"
-  kernel     = "/var/www/cobbler/ks_mirror/Ubuntu-14.04/install/netboot/ubuntu-installer/amd64/linux"
-  initrd     = "/var/www/cobbler/ks_mirror/Ubuntu-14.04/install/netboot/ubuntu-installer/amd64/initrd.gz"
+  for_each                    = { for name, distro in local.distros: name => merge(local.__repos_default_values, distro) }
+    
+    name                      = each.key
+    comment                   = each.value.description
+    
+    breed                     = each.value.breed
+    arch                      = each.value.arch
+    os_version                = each.value.os_version
+    
+    kernel                    = each.value.boot.kernel
+    initrd                    = each.value.boot.initrd
+    boot_files                = join(" ", each.value.boot.other)
+    kernel_options            = join(" ", [ for name, value in each.value.boot.options: format("%s=%s", name, value) ] )
+      
+    mgmt_classes              = join(" ", each.value.cms.roles)
+    template_files            = join(" ", [ for template, file in each.value.cms.templates: format("%s=%s", template, file) ] )
+      
+    redhat_management_key     = each.value.redhat_management.key
+    redhat_management_server  = each.value.redhat_management.server
 }
